0992f7922da276e2dd72864b718442c49dedddd8
sieadev <contact@siea.dev>
2026-02-23 08:26:59 +0100

    added Javadocs Descriptor classes

--- src/main/java/dev/siea/jonion/descriptor/finder/YamlDescriptorFinder.java
+++ src/main/java/dev/siea/jonion/descriptor/finder/YamlDescriptorFinder.java
@@ -13,17 +13,35 @@ import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
+/**
+ * Finds and parses a plugin descriptor from a YAML file inside the plugin JAR.
+ * <p>
+ * Looks for a file (default {@code plugin.yml}) at the JAR root. Expected keys include
+ * {@code name}, {@code main}, {@code version}, {@code description}, {@code authors},
+ * {@code license}, and optionally {@code dependencies} (map of plugin ID to optional boolean).
+ * </p>
+ *
+ * @see PluginDescriptorFinder
+ * @see PluginDescriptor
+ */
 public class YamlDescriptorFinder implements PluginDescriptorFinder {
     private final String descriptorFileName;
 
+    /** Creates a finder that looks for {@code plugin.yml} in the JAR. */
     public YamlDescriptorFinder() {
         this("plugin.yml");
     }
 
+    /**
+     * Creates a finder that looks for the given descriptor file name in the JAR.
+     *
+     * @param descriptorFileName the name of the YAML file (e.g. {@code plugin.yml})
+     */
     public YamlDescriptorFinder(String descriptorFileName) {
         this.descriptorFileName = descriptorFileName;
     }
 
+    @Override
     public PluginDescriptor findPluginDescriptor(Path path) {
         YamlConfiguration yamlConfig = new YamlConfiguration();
         try (JarFile jarFile = new JarFile(path.toFile())) {
