d2b23683c362c7756c2635cbd93f8153940a76ba
sieadev <contact@siea.dev>
2026-02-23 04:46:16 +0100

    Replaced deprecated method `newInstance() with #getDeclaredConstructor().newInstance()

--- src/main/java/dev/siea/jonion/PluginFactory.java
+++ src/main/java/dev/siea/jonion/PluginFactory.java
@@ -4,11 +4,16 @@ import dev.siea.jonion.descriptor.PluginDescriptor;
 import dev.siea.jonion.exceptions.PluginLoadException;
 import dev.siea.jonion.loader.CustomClassLoader;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class PluginFactory {
     public static Plugin createPlugin(PluginDescriptor descriptor, CustomClassLoader classLoader) throws PluginLoadException {
         try {
             Class<?> pluginClass = classLoader.loadClass(descriptor.getPluginClass());
-            return (Plugin) pluginClass.newInstance();
+            return (Plugin) pluginClass.getDeclaredConstructor().newInstance();
+        } catch (InvocationTargetException e) {
+            Throwable cause = e.getCause() != null ? e.getCause() : e;
+            throw new PluginLoadException("Failed to create plugin", cause);
         } catch (Throwable e) {
             throw new PluginLoadException("Failed to create plugin", e);
         }
